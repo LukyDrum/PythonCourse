2dc7b4d75d2e8c33ddbdbaec9097fe37f9d97ccd
Lukas Bubinek <bubinluk@fit.cvut.cz>
2023-06-28 12:38:52 +0200

    Improve code editor

--- static/pythonEditor.js
+++ static/pythonEditor.js
@@ -7,7 +7,26 @@ function runCode() {
     document.getElementsByClassName("py-repl-run-button")[0].dispatchEvent(new MouseEvent("click"));
 }
 
+function downloadCode() {
+    code = document.getElementById("my-repl").getPySrc();
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(code));
+    element.setAttribute('download', "pythonCode.py");
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
 
 document.getElementById("runCodeBtn").addEventListener("click", (event) => {
     runCode();
+});
+
+document.getElementById("downloadBtn").addEventListener("click", (event) => {
+    downloadCode();
 });
