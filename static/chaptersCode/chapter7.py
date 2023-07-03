70ebabcaf56c618d00ac5a0b45102b154c41a4f7
Lukas Bubinek <bubinluk@fit.cvut.cz>
2023-07-03 18:13:43 +0200

    Add new chapter 7

---
+++ static/chaptersCode/chapter7.py
@@ -0,0 +1,10 @@
+# List of integers
+numbers = [69, 420, 42, 12, -5, 666]
+
+print("Printing list:")
+for num in numbers:
+  print(num)
+
+print("Printing range:")
+for i in range(1, 6):
+  print(i)
